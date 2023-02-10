a59a4bcf55a56758026fd44ece543a066b0e258a
Anton Tyurin <anttyurnik@gmail.com>
2023-02-10 12:30:02 +0600

    Image PickerController

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -20,7 +20,25 @@ class NewPlaceViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         if indexPath.row == 0 { // eli index = 0 (kartinka) to nichego ne delaem
+            let actionSheet = UIAlertController(title: nil,
+                                                message: nil,
+                                                preferredStyle: .actionSheet) // sozdaem AlertController s nijnim menyu
             
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in // knopka kamera
+                self.chooseImagePicker(source: .camera)
+            }
+            
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in // knopka photo
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel) // knopka cancel
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            self.present(actionSheet, animated: true)
         } else {
             view.endEditing(true) // esli drugoi index to skryvaem klaviaturu
         }
@@ -36,5 +54,21 @@ extension NewPlaceViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+}
+
+// MARK: - Work with image
+
+extension NewPlaceViewController {
+    
+    // UIImagePickerController - upravlyaet interfeisami dlya s'emki foto, zapisi video, ivybor izobrajeniya iz mediateki pol'zovatelya
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            self.present(imagePicker, animated: true)
+        }
     }
 }
